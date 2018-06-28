2e41542b4e5621a237127715944a8f1ac907ec63
Sander van de Velde <svelde@gmail.com>
2018-06-28 23:19:38 +0200

    code changes of GA reflected

--- Program.cs
+++ Program.cs
@@ -10,20 +10,15 @@ namespace iot.edge.heartbeat
     using System.Threading.Tasks;
     using Microsoft.Azure.Devices.Client;
     using Microsoft.Azure.Devices.Client.Transport.Mqtt;
+
     using Microsoft.Azure.Devices.Shared;
-    using Newtonsoft.Json;
+    using Newtonsoft.Json; 
 
     class Program
     {
         static void Main(string[] args)
         {
-            // The Edge runtime gives us the connection string we need -- it is injected as an environment variable
-            string connectionString = Environment.GetEnvironmentVariable("EdgeHubConnectionString");
-
-            // Cert verification is not yet fully functional when using Windows OS for the container
-            bool bypassCertVerification = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-            if (!bypassCertVerification) InstallCert();
-            Init(connectionString, bypassCertVerification).Wait();
+            Init().Wait();
 
             // Wait until the app unloads or is cancelled
             var cts = new CancellationTokenSource();
@@ -42,50 +37,14 @@ namespace iot.edge.heartbeat
             return tcs.Task;
         }
 
-        /// <summary>
-        /// Add certificate in local cert store for use by client for secure connection to IoT Edge runtime
-        /// </summary>
-        static void InstallCert()
+        static async Task Init()
         {
-            string certPath = Environment.GetEnvironmentVariable("EdgeModuleCACertificateFile");
-            if (string.IsNullOrWhiteSpace(certPath))
-            {
-                // We cannot proceed further without a proper cert file
-                Console.WriteLine($"Missing path to certificate collection file: {certPath}");
-                throw new InvalidOperationException("Missing path to certificate file.");
-            }
-            else if (!File.Exists(certPath))
-            {
-                // We cannot proceed further without a proper cert file
-                Console.WriteLine($"Missing path to certificate collection file: {certPath}");
-                throw new InvalidOperationException("Missing certificate file.");
-            }
-            X509Store store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
-            store.Open(OpenFlags.ReadWrite);
-            store.Add(new X509Certificate2(X509Certificate2.CreateFromCertFile(certPath)));
-            Console.WriteLine("Added Cert: " + certPath);
-            store.Close();
-        }
-
-
-        /// <summary>
-        /// Initializes the DeviceClient and sets up the callback to receive
-        /// messages containing temperature information
-        /// </summary>
-        static async Task Init(string connectionString, bool bypassCertVerification = false)
-        {
-            Console.WriteLine("Connection String {0}", connectionString);
-
             MqttTransportSettings mqttSetting = new MqttTransportSettings(TransportType.Mqtt_Tcp_Only);
-            // During dev you might want to bypass the cert verification. It is highly recommended to verify certs systematically in production
-            if (bypassCertVerification)
-            {
-                mqttSetting.RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
-            }
+
             ITransportSettings[] settings = { mqttSetting };
 
             // Open a connection to the Edge runtime
-            DeviceClient ioTHubModuleClient = DeviceClient.CreateFromConnectionString(connectionString, settings);
+            ModuleClient ioTHubModuleClient = await ModuleClient.CreateFromEnvironmentAsync(settings);
 
             // Attach callback for Twin desired properties updates
             await ioTHubModuleClient.SetDesiredPropertyUpdateCallbackAsync(onDesiredPropertiesUpdate, ioTHubModuleClient);
@@ -100,24 +59,23 @@ namespace iot.edge.heartbeat
 
             var thread = new Thread(() => ThreadBody(ioTHubModuleClient));
             thread.Start();
-
         }
 
         private static async void ThreadBody(object userContext)
         {
             while (true)
             {
-                var deviceClient = userContext as DeviceClient;
+                var client = userContext as ModuleClient;
 
-                if (deviceClient == null)
+                if (client == null)
                 {
                     throw new InvalidOperationException("UserContext doesn't contain " + "expected values");
                 }
 
                 var heartbeatMessageBody = new HeartbeatMessageBody
-                                {
-                                    timeCreated = DateTime.Now.ToString("hh:mm:ss")
-                                };
+                {
+                    timeCreated = DateTime.Now.ToString("hh:mm:ss")
+                };
 
                 var jsonMessage = JsonConvert.SerializeObject(heartbeatMessageBody);
 
@@ -125,7 +83,7 @@ namespace iot.edge.heartbeat
 
                 pipeMessage.Properties.Add("content-type", "application/edge-heartbeat-json");
 
-                await deviceClient.SendEventAsync("output1", pipeMessage);
+                await client.SendEventAsync("output1", pipeMessage);
 
                 Console.WriteLine($"Heartbeat sent {heartbeatMessageBody.timeCreated}");
 
@@ -172,8 +130,8 @@ namespace iot.edge.heartbeat
             {
                 foreach (Exception exception in ex.InnerExceptions)
                 {
-                    Console.WriteLine();
-                    Console.WriteLine("Error when receiving desired property: {0}", exception);
+                Console.WriteLine();
+                Console.WriteLine("Error when receiving desired property: {0}", exception);
                 }
             }
             catch (Exception ex)
